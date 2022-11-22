51461d42dc5030224a53a70a4bf3fda23e7145d4
Bengi Genç Bıyık <genc.bengi@gmail.com>
2022-11-22 14:09:42 +0300

    Add new group form with a style

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,26 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @groups = current_user.groups if user_signed_in?
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+    if @group.save
+      redirect_to groups_path, notice: "New group created succefssfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
