bbf08b3c72603101ae530cad40357939169e216d
Bengi Genç Bıyık <genc.bengi@gmail.com>
2022-11-23 17:55:53 +0300

    Test expense request

--- spec/request/expense_spec.rb
+++ spec/request/expense_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Groups', type: :request do
+RSpec.describe 'Expenses', type: :request do
   describe 'GET #index' do
     before(:each) do
       @user = User.create(name: 'testUser', email: 'user@mail.com', password: 'password')
       @group = Group.create(name: 'testGroup', icon: 'dndndndndndndn')
+      @expense = Expense.create(name: 'testExpense', amount: 100, user_id: @user.id)
     end
-
     it 'is an HTTP request' do
-      get groups_path
+      get @expense_path
       expect(response).to have_http_status(200)
     end
     it 'renders the index template' do
-      get groups_path
+      get @expense_path
       expect(response).to render_template(:index)
     end
   end
