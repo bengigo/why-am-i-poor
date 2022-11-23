967084d08a9ff623fe903a3cbab63c97f306d728
Bengi Genç Bıyık <genc.bengi@gmail.com>
2022-11-23 17:28:21 +0300

    Test group requests

---
+++ spec/request/group_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Groups", type: :request do
+  describe 'GET #index' do
+
+    # before do
+    #   @user = User.create(name: 'testUser', email: 'user@mail.com', password: 'password')
+    #   @group = Group.create(name: 'testGroup', icon: 'dndndndndndndn')
+    #   sign_in @user
+    # end
+    before(:each) do
+      @user = User.create(name: 'testUser', email: 'user@mail.com', password: 'password')
+      @group = Group.create(name: 'testGroup', icon: 'dndndndndndndn')
+    end
+
+
+    it 'is an HTTP request' do
+      get groups_path
+      expect(response).to have_http_status(200)
+    end
+    it 'renders the index template' do
+      get groups_path
+      expect(response).to render_template(:index)
+    end
+  end
+end
