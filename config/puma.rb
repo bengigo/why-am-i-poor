8bb3cccf143e78ced41377ca734c00366b5e3e3f
Bengi Genç Bıyık <genc.bengi@gmail.com>
2022-11-25 11:50:02 +0300

    Add build script

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,7 @@ port ENV.fetch('PORT', 3000)
 environment ENV.fetch('RAILS_ENV', 'development')
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -33,11 +33,13 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
+This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
+process behavior so workers use less memory.
 #
 # preload_app!
 
